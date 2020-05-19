61d15a56869960c11b3a99dfa8e64286253b6ff9
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-18 23:56:44 -0500

    Definiendo el AppState de nuestra

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,7 +2,9 @@ import { createReducer, on } from '@ngrx/store';
 import { crear } from './todo.actions';
 import { Todo } from './models/todo.model';
 
-export const estadoInicial: Todo[] = [];
+export const estadoInicial: Todo[] = [
+  new Todo('Salvar al mundo')
+];
 
 // tslint:disable-next-line: variable-name
 const _todoReducer = createReducer(estadoInicial,
