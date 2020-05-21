9e14be29b7ee445a6e545e3d87bf69e2910ad636
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-20 23:42:36 -0500

    Toggle All - Marcar o Desmarcar Todos

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { crear, toggle, editar, borrar } from './todo.actions';
+import { crear, toggle, editar, borrar, toggleAll } from './todo.actions';
 import { Todo } from './models/todo.model';
 
 export const estadoInicial: Todo[] = [
@@ -14,6 +14,13 @@ const _todoReducer = createReducer(estadoInicial,
   on(crear, (state, { texto }) => [...state, new Todo(texto)]),
 
   on(borrar, (state, { id }) => state.filter((todo) => todo.id !== id)),
+
+  on(toggleAll, (state, { completado }) => state.map(todo => {
+    return {
+      ...todo,
+      completado
+    };
+  })),
 
   on(toggle, (state, { id }) => {
     return state.map(todo => {
